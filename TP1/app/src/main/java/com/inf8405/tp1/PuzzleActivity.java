645fc01ac6a409e9ed9f34270e7215d03f6059c6
Philippe-Savard <65316883+Philippe-Savard@users.noreply.github.com>
2022-01-31 01:16:11 -0500

    Added OnTouchListener for onTouch handling.

--- TP1/app/src/main/java/com/inf8405/tp1/PuzzleActivity.java
+++ TP1/app/src/main/java/com/inf8405/tp1/PuzzleActivity.java
@@ -1,18 +1,33 @@
 package com.inf8405.tp1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class PuzzleActivity extends AppCompatActivity {
+public class PuzzleActivity extends AppCompatActivity implements View.OnTouchListener{
 
+    ImageView _star;
+    ViewGroup _board;
+    private int _dx;
+    private int _dy;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_page);
         hideToolBr();
+
+        _board = (ViewGroup) findViewById(R.id.layout_middle_gameboard);
+        _star = findViewById(R.id.imageView);
+        _star.setOnTouchListener(this);
     }
 
     public void hideToolBr() {
@@ -24,6 +39,33 @@ public class PuzzleActivity extends AppCompatActivity {
         uiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public boolean onTouch(View view, MotionEvent event) {
+
+        final int x = (int) event.getRawX();
+        final int y = (int) event.getRawY();
+
+        ConstraintLayout.LayoutParams lParams = (ConstraintLayout.LayoutParams) view.getLayoutParams();
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                // The player touches the view on screen
+                _dx = x - lParams.leftMargin;
+                _dy = y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // The player start moving the view on screen
+                lParams.leftMargin = x - _dx;
+                lParams.topMargin = y - _dy;
+                view.setLayoutParams(lParams);
+                break;
+            default:
+                break;
+        }
+        _board.invalidate();
+        return true;
     }
 
     public void onButtonPauseClick(View view) {
